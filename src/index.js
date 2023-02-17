c4de089ad3de28021ce16802c4403cf30506cee9
Stanislau Virsky <vyr4376@yandex.ru>
2023-02-17 21:25:44 +0300

    feat:finish work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+
+  for (let b = 0; b < str.length; b++) {
+    bracketsConfig.forEach((brack) => {
+      brack = brack.join("");
+      if (str.includes(brack)) {
+        str = str.replace(brack, "");
+        b = 0;
+      }
+    });
+  }
+
+  return str.length === 0;
+};
